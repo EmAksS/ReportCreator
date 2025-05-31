49df821f783a4a7a479575533e25a494b529d898
Alex Jienexa <alexjienexa@gmail.com>
2025-05-31 13:31:33 +0300

    CSRF2

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -15,18 +15,18 @@ CSRF_COOKIE_HTTPONLY = False   # Позволяет JavaScript читать CSRF
 FRONTEND_PORT = environ.get('FRONTEND_PORT', 3000)
 
 # Хосты для связи фронта и бека (CORS)
-# CORS_ALLOWED_ORIGINS = [
-#     f'http://localhost:{FRONTEND_PORT}', # React server
-#     f'http://127.0.0.1:{FRONTEND_PORT}',
-#     f"http://26.213.134.143:{FRONTEND_PORT}",
-#     f"http://26.135.149.127:{FRONTEND_PORT}",
-#     f"http://26.135.149.127:8000",
-#     f"http://26.146.126.232:{FRONTEND_PORT}",
-#     f"http://26.146.126.232:8000",
-#     f"http://26.213.134.143:8000",
-#     f"https://26.63.43.26:{FRONTEND_PORT}",
-#     f"https://26.63.43.26:8000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    f'http://localhost:{FRONTEND_PORT}', # React server
+    f'http://127.0.0.1:{FRONTEND_PORT}',
+    f"http://26.213.134.143:{FRONTEND_PORT}",
+    f"http://26.135.149.127:{FRONTEND_PORT}",
+    f"http://26.135.149.127:8000",
+    f"http://26.146.126.232:{FRONTEND_PORT}",
+    f"http://26.146.126.232:8000",
+    f"http://26.213.134.143:8000",
+    f"https://26.63.43.26:{FRONTEND_PORT}",
+    f"https://26.63.43.26:8000",
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
